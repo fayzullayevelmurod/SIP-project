ac2fb5ef011a1f77ede42c5e454ca85a9fa46fba
SardorQobilov <sardorqobilov555@gmail.com>
2024-08-24 10:36:26 +0500

    tree pages media finished

--- src/pages/data-tree/DataTree.jsx
+++ src/pages/data-tree/DataTree.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./data-tree.scss";
 import { Header } from "../../layouts/Header";
 import { ShowNavContext } from "../../context/ShowNavContext";
@@ -9,6 +9,8 @@ import { IconSearchStroked } from "@consta/icons/IconSearchStroked";
 import { IconSortDownCenter } from "@consta/icons/IconSortDownCenter";
 import { TreeComponent } from "./Tree";
 import assets from "../../assets";
+import { IconHamburger } from "@consta/icons/IconHamburger";
+import { IconClose } from "@consta/icons/IconClose";
 
 const pagesLink = [
   {
@@ -23,6 +25,12 @@ const pagesLink = [
 
 const DataTree = () => {
   const { handleToggleNav, activeNav } = useContext(ShowNavContext);
+  const [showNav, setShowNav] = useState(false);
+
+  const handleToggleShowNav = () => {
+    setShowNav(!showNav);
+  };
+
   return (
     <div className="data-tree__page">
       <Header
@@ -33,7 +41,7 @@ const DataTree = () => {
         dropdownLogo={true}
         pagesLink={pagesLink}
       />
-      <div className="flex">
+      <div className="flex relative-box">
         <NavbarLayout
           navbarHidden={true}
           hideHamburger={true}
@@ -41,7 +49,11 @@ const DataTree = () => {
           setActiveNav={activeNav}
         />
         <div className="sidebars">
-          <div className="navbar-sidebar">
+          <div
+            className={`navbar-sidebar media-hide__navbar ${
+              showNav ? "show" : ""
+            }`}
+          >
             <div className="sidebar-header">
               <Text as="h1" view="primary">
                 Дерево данных
@@ -49,11 +61,24 @@ const DataTree = () => {
               <div className="sidebar-header__btns">
                 <Button size="xs" view="clear" iconLeft={IconSearchStroked} />
                 <Button size="xs" view="clear" iconLeft={IconSortDownCenter} />
+                <Button
+                  size="xs"
+                  view="ghost"
+                  iconLeft={IconClose}
+                  onClick={handleToggleShowNav}
+                />
               </div>
             </div>
             <TreeComponent />
           </div>
           <div className="no-data">
+            <Button
+              onClick={handleToggleShowNav}
+              className="media-show__nav-btn"
+              size="xs"
+              view="ghost"
+              iconLeft={IconHamburger}
+            />
             <div className="null-content">
               <img src={assets.notFoundImg} alt="" />
               <Text view="primary" size="lg" weight="semibold">
--- src/pages/data-tree/MultiMenu.jsx
+++ src/pages/data-tree/MultiMenu.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Header } from "../../layouts/Header";
 import { ShowNavContext } from "../../context/ShowNavContext";
 import { NavbarLayout } from "../../components/Navbar";
@@ -13,6 +13,8 @@ import { IconSeismic2D } from "@consta/icons/IconSeismic2D";
 import { IconMarkers } from "@consta/icons/IconMarkers";
 import { IconMaps } from "@consta/icons/IconMaps";
 import { IconArrowRight } from "@consta/icons/IconArrowRight";
+import { IconClose } from "@consta/icons/IconClose";
+import { IconHamburger } from "@consta/icons/IconHamburger";
 
 const pagesLink = [
   {
@@ -38,6 +40,11 @@ const pagesLink2 = [
 
 const MultiMenu = () => {
   const { handleToggleNav, activeNav } = useContext(ShowNavContext);
+  const [showNav, setShowNav] = useState(false);
+
+  const handleToggleShowNav = () => {
+    setShowNav(!showNav);
+  };
   return (
     <div className="data-tree__page multi-menu__page">
       <Header
@@ -48,7 +55,7 @@ const MultiMenu = () => {
         dropdownLogo={true}
         pagesLink={pagesLink}
       />
-      <div className="flex">
+      <div className="flex relative-box">
         <NavbarLayout
           navbarHidden={true}
           hideHamburger={true}
@@ -56,12 +63,23 @@ const MultiMenu = () => {
           setActiveNav={activeNav}
         />
         <div className="sidebars">
-          <div className="navbar-sidebar">
+          <div
+            className={`navbar-sidebar media-hide__navbar ${
+              showNav ? "show" : ""
+            }`}
+          >
             <div className="sidebar-header">
               <CustomBreadcrumbs items={pagesLink2} />
               <div className="sidebar-header__btns">
                 <Button size="xs" view="clear" iconLeft={IconSearchStroked} />
                 <Button size="xs" view="clear" iconLeft={IconSortDownCenter} />
+                <Button
+                  size="xs"
+                  view="ghost"
+                  iconLeft={IconClose}
+                  onClick={handleToggleShowNav}
+                  className="media-show__menu-btn"
+                />
               </div>
             </div>
             <ul className="list">
@@ -122,6 +140,13 @@ const MultiMenu = () => {
             </ul>
           </div>
           <div className="no-data">
+            <Button
+              onClick={handleToggleShowNav}
+              className="media-show__nav-btn"
+              size="xs"
+              view="ghost"
+              iconLeft={IconHamburger}
+            />
             <div className="null-content">
               <img src={assets.notFoundImg} alt="" />
               <Text view="primary" size="lg" weight="semibold">
